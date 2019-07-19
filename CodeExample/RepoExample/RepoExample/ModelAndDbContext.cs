d925e1ac55e4436920d3d8dbcec922fd2d9903f2
tramtln <vuanlbv@gmail.com>
2019-07-19 11:33:59 +0700

    asd

---
+++ CodeExample/RepoExample/RepoExample/ModelAndDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoExample
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+
+    public class DbStudentContext : DbContext
+    {
+        public DbStudentContext()
+        {
+
+        }
+        public virtual DbSet<Student> Forms { get; set; }
+    }
+}
---
+++ CodeExample/RepoExample/RepoExample/UnitOfWork.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RepoExample
+{
+    public class UnitOfWork
+    {
+        public UnitOfWork()
+        {
+            _dbContext = new DbStudentContext();
+            Students = new Repository<Student>(_dbContext);
+        }
+
+        #region Propeties
+        DbContext _dbContext;
+        public Repository<Student> Students { get; set; }
+        #endregion
+        
+        #region Methods
+
+        /// <summary>
+        ///     Save changes into database.
+        /// </summary>
+        /// <returns></returns>
+        public int Commit()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        ///     Save changes into database asynchronously.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<int> CommitAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        /// <summary>
+        ///     Begin transaction scope.
+        /// </summary>
+        /// <returns></returns>
+        public DbContextTransaction BeginTransactionScope()
+        {
+            return _dbContext.Database.BeginTransaction();
+        }
+
+        /// <summary>
+        ///     Begin transaction scope.
+        /// </summary>
+        /// <param name="isolationLevel"></param>
+        /// <returns></returns>
+        public DbContextTransaction BeginTransactionScope(IsolationLevel isolationLevel)
+        {
+            return _dbContext.Database.BeginTransaction(isolationLevel);
+        }
+
+        #endregion
+    }
+}
